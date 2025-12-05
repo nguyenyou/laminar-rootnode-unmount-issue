690b7c4c6f1caa4a99a60aa24923a96ec3d41802
nguyenyou <tunt081295@gmail.com>
2025-12-05 12:05:00 +0700

    🚢 ship it

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.PAGES_BASE_PATH || '/',
+  plugins: [tailwindcss()],
+  base: process.env.PAGES_BASE_PATH || "/",
   build: {
-    minify: false
-  }
-})
+    minify: false,
+  },
+});
